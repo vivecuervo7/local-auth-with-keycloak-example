b983b803d4b8eac20ab89e2961ef4e3027b2498f
Isaac Dedini <vivecuervo7@gmail.com>
2024-08-06 12:53:19 +1000

    Simplify setup so user need only create the missing certificates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,16 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
+export default defineConfig(({ command }) => {
   if (command === "serve") {
     return {
       plugins: [react()],
       server: {
         port: 5173,
         https: {
-          cert: process.env.VITE_CERT ?? "",
-          key: process.env.VITE_CERT_KEY ?? "",
+          cert: "../local-dev/certificates/cert.pem",
+          key: "../local-dev/certificates/key.pem",
         },
       },
     };
